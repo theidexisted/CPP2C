624deeb3e246d32e26ff5441af364c3eda2589d6
ppenguin <invalid_ms_user@live.com>
2020-08-28 09:37:26 +0800

    Enum runnable

--- CPP2C.cpp
+++ CPP2C.cpp
@@ -13,11 +13,14 @@
 #include <clang/Tooling/Tooling.h>
 #include <iostream>
 #include <llvm/ADT/StringExtras.h>
+#include <llvm/Support/Debug.h>
 #include <map>
 #include <set>
 #include <sstream>
 #include <tuple>
 #include <vector>
+
+#define DEBUG_TYPE "promethus_gen"
 
 using namespace std;
 using namespace clang;
@@ -32,285 +35,111 @@ static cl::opt<std::string> OutputFilename(
     "o", cl::desc(getDriverOptTable().getOptionHelpText((options::OPT_o))),
     cl::cat(CPP2CCategory));
 
+static cl::opt<bool, true> Debug("debug", cl::desc("Enable debug output"),
+                                 cl::location(DebugFlag),
+                                 cl::cat(CPP2CCategory));
+
 static cl::opt<std::string> ClassesToGenrate(
     "classes", cl::desc("Classes to generate C binding, split by space"),
     cl::init("uThread kThread Cluster Connection Mutex OwnerLock "
              "ConditionVariable Semaphore uThreadPool"),
     cl::cat(CPP2CCategory));
 
-/** Classes to be mapped to C **/
-struct OutputStreams {
-  string headerString;
-  string bodyString;
-
-  llvm::raw_string_ostream HeaderOS;
-  llvm::raw_string_ostream BodyOS;
-
-  OutputStreams() : HeaderOS(headerString), BodyOS(bodyString){};
-};
-
 llvm::SmallVector<llvm::StringRef, 16> ClassList;
 map<string, int> funcList;
 
-/** Matchers **/
-
-/** Handlers **/
-class classMatchHandler : public MatchFinder::MatchCallback {
+class EnumMatchHandler : public MatchFinder::MatchCallback {
 public:
-  classMatchHandler(OutputStreams &os) : OS(os) {}
+  EnumMatchHandler() = default;
 
-  tuple<string, string, bool, bool> determineCType(const QualType &qt) {
-
-    string CType;
-    string CastType; // whether this should be casted or not
-    bool isPointer = false;
-    bool shoulReturn = true;
-
-    // if it is builtint type use it as is
-    if (qt->isBuiltinType() ||
-        (qt->isPointerType() && qt->getPointeeType()->isBuiltinType())) {
-      CType = qt.getAsString();
-      if (qt->isVoidType())
-        shoulReturn = false;
-      // if it is a CXXrecordDecl then return a pointer to WName*
-    } else if (qt->isRecordType()) {
-      const CXXRecordDecl *crd = qt->getAsCXXRecordDecl();
-      string recordName = crd->getNameAsString();
-      CType = "W" + recordName + "*";
-      CastType = recordName + "*";
-
-    } else if ((qt->isReferenceType() || qt->isPointerType()) &&
-               qt->getPointeeType()->isRecordType()) {
-      isPointer = true; // to properly differentiate among cast types
-      const CXXRecordDecl *crd = qt->getPointeeType()->getAsCXXRecordDecl();
-      string recordName = crd->getNameAsString();
-      if (std::find(ClassList.begin(), ClassList.end(), recordName) !=
-          ClassList.end()) {
-        CType = "W" + recordName + "*";
-        CastType = recordName + "*";
-      } else {
-        CType = recordName + "*";
-      }
+  struct Property {
+    std::string name;
+    std::vector<std::string> labels;
+    std::string ToString() {
+      return "name:" + name +
+             ",labels:" + llvm::join(labels.begin(), labels.end(), ",");
     }
-    return make_tuple(CType, CastType, isPointer, shoulReturn);
-  }
+  };
 
+  bool ParseAnnotation(const Attr *attr, Property &prop) {
+    llvm::SmallString<64> str;
+    llvm::raw_svector_ostream os(str);
+    LangOptions langopts;
+    PrintingPolicy policy(langopts);
+    attr->printPretty(os, policy);
+
+    auto left = str.find('\"');
+    auto right = str.rfind('\"');
+    if (left == llvm::StringRef::npos || right == llvm::StringRef::npos ||
+        left >= right) {
+      LLVM_DEBUG(
+          llvm::dbgs()
+          << "The attr content parse failed, no \" founded or no content\n");
+      return false;
+    }
+    llvm::StringRef content = str.slice(left + 1, right);
+
+    llvm::SmallVector<llvm::StringRef, 4> valuePair;
+    llvm::SplitString(content, valuePair, ":");
+
+    assert(!valuePair.empty());
+
+    if (valuePair[0] == "name") {
+      prop.name = valuePair[1];
+    } else if (valuePair[0] == "labels") {
+      llvm::SmallVector<llvm::StringRef, 4> labels;
+      llvm::SplitString(valuePair[1], labels, ",");
+      prop.labels = {labels.begin(), labels.end()};
+    } else {
+      LLVM_DEBUG(llvm::dbgs()
+                 << "Can't parse the content, no name or labels pairs found\n");
+      return false;
+    }
+
+    return true;
+  }
   virtual void run(const MatchFinder::MatchResult &Result) {
-    if (const CXXMethodDecl *cmd =
-            Result.Nodes.getNodeAs<CXXMethodDecl>("publicMethodDecl")) {
-      string methodName;
-      string className = cmd->getParent()->getDeclName().getAsString();
-      string returnType;
-      string returnCast;
-      bool shouldReturn, isPointer;
-      string self = "W" + className + "* self";
-      string separator = ", ";
-      string bodyEnd;
-
-      std::stringstream functionBody;
-
-      // ignore operator overloadings
-      if (cmd->isOverloadedOperator())
-        return;
-
-      // constructor
-      if (const CXXConstructorDecl *ccd = dyn_cast<CXXConstructorDecl>(cmd)) {
-        if (ccd->isCopyConstructor() || ccd->isMoveConstructor())
-          return;
-        methodName = "_create";
-        returnType = "W" + className + "*";
-        self = "";
-        separator = "";
-        functionBody << "return reinterpret_cast<" << returnType << ">( new "
-                     << className << "(";
-        bodyEnd += "))";
-      } else if (isa<CXXDestructorDecl>(cmd)) {
-        methodName = "_destroy";
-        returnType = "void";
-        functionBody << " delete reinterpret_cast<" << className << "*>(self)";
-      } else {
-        methodName = "_" + cmd->getNameAsString();
-        const QualType qt = cmd->getReturnType();
-        std::tie(returnType, returnCast, isPointer, shouldReturn) =
-            determineCType(qt);
-
-        // should this function return?
-        if (shouldReturn)
-          functionBody << "return ";
-
-        if (returnCast != "") {
-          // if not pointer and it needs to be casted, then return the pointer
-          if (!isPointer)
-            functionBody << "&";
-          functionBody << "reinterpret_cast<" << returnType << ">(";
-          bodyEnd += ")";
+    if (const EnumDecl *cmd = Result.Nodes.getNodeAs<EnumDecl>("enum")) {
+      LLVM_DEBUG(llvm::dbgs() << "Match an enum:"
+                              << cmd->getQualifiedNameAsString() << '\n');
+      for (const auto &enumConstant : cmd->enumerators()) {
+        Property prop;
+        for (const auto &attr : enumConstant->attrs()) {
+          if (attr->getKind() == attr::Annotate) {
+            ParseAnnotation(attr, prop);
+          }
         }
-
-        // if Static call it properly
-        if (cmd->isStatic())
-          functionBody << className << "::" << cmd->getNameAsString() << "(";
-        // if not  use the passed object to call the method
-        else
-          functionBody << "reinterpret_cast<" << className << "*>(self)->"
-                       << cmd->getNameAsString() << "(";
-
-        bodyEnd += ")";
+        LLVM_DEBUG(llvm::dbgs()
+                   << "For enum:" << enumConstant->getQualifiedNameAsString()
+                   << ' ' << "label parsed:" << prop.ToString() << '\n');
       }
-
-      std::stringstream funcname;
-      funcname << returnType << " " << className << methodName;
-
-      auto it = funcList.find(funcname.str());
-
-      if (it != funcList.end()) {
-        it->second++;
-        funcname << "_" << it->second;
-      } else {
-        funcList[funcname.str()] = 0;
-      }
-
-      funcname << "(" << self;
-
-      for (unsigned int i = 0; i < cmd->getNumParams(); i++) {
-        const QualType qt = cmd->parameters()[i]->getType();
-        std::tie(returnType, returnCast, isPointer, shouldReturn) =
-            determineCType(qt);
-        funcname << separator << returnType << " ";
-        funcname << cmd->parameters()[i]->getQualifiedNameAsString() << "";
-
-        if (i != 0)
-          functionBody << separator;
-        if (returnCast == "")
-          functionBody << cmd->parameters()[i]->getQualifiedNameAsString();
-        else {
-          if (!isPointer)
-            functionBody << "*";
-          functionBody << "reinterpret_cast<" << returnCast << ">("
-                       << cmd->parameters()[i]->getQualifiedNameAsString()
-                       << ")";
-        }
-
-        string separator = ", ";
-      }
-      funcname << ")";
-
-      OS.HeaderOS << funcname.str() << ";\n";
-
-      OS.BodyOS << funcname.str() << "{\n    ";
-      OS.BodyOS << functionBody.str();
-      OS.BodyOS << bodyEnd << "; \n}\n";
     }
   }
-  virtual void onEndOfTranslationUnit() {}
-
-private:
-  OutputStreams &OS;
 };
 
-/****************** /Member Functions *******************************/
-// Implementation of the ASTConsumer interface for reading an AST produced
-// by the Clang parser. It registers a couple of matchers and runs them on
-// the AST.
-class MyASTConsumer : public ASTConsumer {
+class EnumASTConsumer : public ASTConsumer {
 public:
-  MyASTConsumer(OutputStreams &os) : OS(os), HandlerForClassMatcher(os) {
-    // Add a simple matcher for finding 'if' statements.
-
-    for (const std::string &className : ClassList) {
-      OS.HeaderOS << "struct      W" << className
-                  << "; \n"
-                     "typedef     struct W"
-                  << className << " W" << className << ";\n";
-      // oss.push_back(std::move(os))
-
-      DeclarationMatcher classMatcher =
-          cxxMethodDecl(isPublic(), ofClass(hasName(className)))
-              .bind("publicMethodDecl");
-      Matcher.addMatcher(classMatcher, &HandlerForClassMatcher);
-    }
+  EnumASTConsumer() {
+    DeclarationMatcher enumMatcher = enumDecl(isDefinition()).bind("enum");
+    Matcher.addMatcher(enumMatcher, &handler);
   }
 
   void HandleTranslationUnit(ASTContext &Context) override {
-    // Run the matchers when we have the whole TU parsed.
     Matcher.matchAST(Context);
   }
 
 private:
-  OutputStreams &OS;
-  classMatchHandler HandlerForClassMatcher;
-
+  EnumMatchHandler handler;
   MatchFinder Matcher;
 };
 
-// For each source file provided to the tool, a new FrontendAction is created.
 class MyFrontendAction : public ASTFrontendAction {
 public:
-  MyFrontendAction() {
-    OS.HeaderOS << "#ifndef UTHREADS_CWRAPPER_H\n"
-                   "#define UTHREADS_CWRAPPER_H_\n"
-                   "#include <pthread.h>\n"
-                   "#include <sys/types.h>\n"
-                   "#include <sys/socket.h>\n"
-                   "#include <inttypes.h>\n\n"
-
-                   "#ifdef __cplusplus\n"
-                   "extern \"C\"{\n"
-                   "#endif\n"
-                   "#include <stdbool.h>\n";
-    OS.BodyOS << "#include \"generic/basics.h\"\n"
-                 "#include \"cwrapper.h\"\n"
-                 "#include \"runtime/uThread.h\"\n"
-                 "#include \"runtime/uThreadPool.h\"\n"
-                 "#include \"runtime/kThread.h\"\n"
-                 "#include \"io/Network.h\"\n"
-                 "#ifdef __cplusplus\n"
-                 "extern \"C\"{\n"
-                 "#endif\n";
-  }
-
-  void EndSourceFileAction() override {
-
-    StringRef headerFile("cwrapper.h");
-    StringRef bodyFile("cwrapper.cpp");
-
-    // Open the output file
-    std::error_code EC;
-    llvm::raw_fd_ostream HOS(headerFile, EC, llvm::sys::fs::F_None);
-    if (EC) {
-      llvm::errs() << "while opening '" << headerFile << "': " << EC.message()
-                   << '\n';
-      exit(1);
-    }
-    llvm::raw_fd_ostream BOS(bodyFile, EC, llvm::sys::fs::F_None);
-    if (EC) {
-      llvm::errs() << "while opening '" << bodyFile << "': " << EC.message()
-                   << '\n';
-      exit(1);
-    }
-
-    OS.HeaderOS << "#ifdef __cplusplus\n"
-                   "}\n"
-                   "#endif\n"
-                   "#endif /* UTHREADS_CWRAPPER_H_ */\n";
-
-    OS.BodyOS << "#ifdef __cplusplus\n"
-                 "}\n"
-                 "#endif\n";
-
-    OS.HeaderOS.flush();
-    OS.BodyOS.flush();
-    HOS << OS.headerString << "\n";
-    BOS << OS.bodyString << "\n";
-  }
-
   std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
                                                  StringRef file) override {
 
-    return std::make_unique<MyASTConsumer>(OS);
+    return std::make_unique<EnumASTConsumer>();
   }
-
-private:
-  OutputStreams OS;
 };
 
 int main(int argc, const char **argv) {
